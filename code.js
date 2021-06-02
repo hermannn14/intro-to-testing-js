fad3ecc23746457192a2b3d11f45180cb23bcaac
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-06-02 12:27:40 -0500

    first test and solution for intro to testing

--- code.js
+++ code.js
@@ -2,3 +2,12 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(name) {
+    if (name === "Summer") {
+        return "Hello, Summer!"
+    }else if (name === "Obi") {
+        return "Hello, Obi!";
+    }
+    return "Hello, World!";
+}
